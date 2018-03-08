82d4ceb9465b003a85d71bdba6be6deac44bdfce
魏亮 <498516493@qq.com>
2018-03-08 09:58:22 +0800

    Initial Commit

---
+++ zhmz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhmz
+//
+//  Created by 魏亮 on 2018/3/8.
+//  Copyright © 2018年 魏亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ zhmz/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  zhmz
+//
+//  Created by 魏亮 on 2018/3/8.
+//  Copyright © 2018年 魏亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
